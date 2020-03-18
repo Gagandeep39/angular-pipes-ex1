bc44feb0e85389f22c809fde49505ac52d8e4c16
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-03-19 00:43:43 +0530

    Created arameterized pipe

--- src/app/shorten.pipe.ts
+++ src/app/shorten.pipe.ts
@@ -5,7 +5,14 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ShortenPipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
+  // Shortens the pipe to the length specified as parameter
+  // It takes limit as an optional parameter
+  // ere the parameter could have been stored in args
+  // for readability, an optional parameter 'limit' was defined
+  transform(value: any, limit?: number, args?: any): any {
+    if(value.length > limit){
+      return value.substr(0, limit) + '...'
+    }
     return value.substr(0, 10) + '...';
   }
 
